2a5b5306fd71a13b1b36a0c57b7b2d575071646a
LifeDrainingCoding <ancenkokirill104@gmail.com>
2024-04-19 18:35:39 +0900

    added functionality for register button in Register_Activity.java.

--- app/src/main/java/com/kursach/ckursach/Register_Activity.java
+++ app/src/main/java/com/kursach/ckursach/Register_Activity.java
@@ -1,12 +1,16 @@
 package com.kursach.ckursach;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,13 +18,31 @@ import com.google.firebase.auth.FirebaseUser;
 public class Register_Activity extends AppCompatActivity {
     private FirebaseAuth auth;
     public static final String TAG = "Register Activity";
+    private Button regBtn;
+    private EditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         auth = FirebaseAuth.getInstance();
+       regBtn =  (MaterialButton ) findViewById(R.id.register_btn);
+       email =  findViewById(R.id.editTextEmail);
+       password =  findViewById(R.id.editTextTextPassword);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(email.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
     private void createAccount(String email, String password) {
         // [START create_user_with_email]
         auth.createUserWithEmailAndPassword(email, password)
@@ -40,6 +62,7 @@ public class Register_Activity extends AppCompatActivity {
                         }
                     }
                 });
+
         // [END create_user_with_email]
     }
 }
