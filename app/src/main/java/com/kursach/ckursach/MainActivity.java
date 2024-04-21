f1b04e4ee97b05d1ee2ece5be3229f23933c7371
LifeDrainingCoding <ancenkokirill104@gmail.com>
2024-04-21 17:47:02 +0900

    UI corrections, finished  sign in authorization. Since now user panel will show user's email(password WIP ).  Now app will be forced to   authorize in a test account

--- app/src/main/java/com/kursach/ckursach/MainActivity.java
+++ app/src/main/java/com/kursach/ckursach/MainActivity.java
@@ -3,30 +3,55 @@ package com.kursach.ckursach;
 
 import android.content.Intent;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private final static String TAG = "LogIn_Activity";
+    private MaterialButton signInButton;
+    private EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         auth = FirebaseAuth.getInstance();
+        signInButton = findViewById(R.id.login_btn);
+        email = findViewById(R.id.editTextLoginEmail);
+        password =  findViewById(R.id.editTextLoginPassword);
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(email.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+
+
+
 
 
 
     private void signIn(String email, String password) {
         // [START sign_in_with_email]
+        email = "ancenkokirill104@gmail.com";
+        password = "dk137dark3";//todo DELETE THIS IN RELEASE
         auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -39,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Log.e(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
--- app/src/main/java/com/kursach/ckursach/UserPanel_Activity.java
+++ app/src/main/java/com/kursach/ckursach/UserPanel_Activity.java
@@ -1,13 +1,43 @@
 package com.kursach.ckursach;
 
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserPanel_Activity extends AppCompatActivity {
-
+    private Boolean isShowed = false;
+    private  Button showMetadataBtn;
+    private TextView emailView, passwordView;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_panel);
+        emailView = findViewById(R.id.userEmail);
+        passwordView =  findViewById(R.id.userPassword);
+        auth = FirebaseAuth.getInstance();
+        user =  auth.getCurrentUser();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getEmail();
+
+        //TODO: сделать получение пароля  password =   passwordView.getText().toString()+" "+user.get
+
+    }
+    public void getEmail(){
+        if(isShowed == false){
+
+        isShowed=true;
+        String email,password;
+        email = emailView.getText().toString() +" "+user.getEmail();
+        emailView.setText(email);
+        }
     }
 }
